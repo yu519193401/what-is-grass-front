6f2068f55216040eb51c647981ff47f7639bbfb0
shibayaman <shibayaman7374@gmail.com>
2021-06-22 12:12:03 +0900

    Add 質問投稿ページ

--- packages/shared/src/mocks/handlers.ts
+++ packages/shared/src/mocks/handlers.ts
@@ -1,4 +1,8 @@
 import { rest } from 'msw';
 import { getUser } from './resolvers/user';
+import { newQuestion } from './resolvers/question';
 
-export const handlers = [rest.get('/user', getUser)];
+export const handlers = [
+  rest.get('/user', getUser),
+  rest.post('/question', newQuestion),
+];
