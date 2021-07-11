066121fcfff444bbf48f828be484335ed6f39e47
shibayaman <shibayaman7374@gmail.com>
2021-07-12 00:15:59 +0900

    Change 回答一覧取得をrtk-query化

--- packages/web/pages/answers.tsx
+++ packages/web/pages/answers.tsx
@@ -1,30 +1,19 @@
-import { useEffect, useState } from 'react';
 import AnswerList from '../components/AnswerList';
+import { useGetAnswersQuery } from '@what-is-grass/shared';
 
 const QuestionAnswer: React.FC = () => {
-  const [answers, setAnswers] = useState([]);
-
   const getIndexId = () => {
     return 1;
   };
   const indexId = getIndexId();
-
-  useEffect(() => {
-    const callAnswer = async () => {
-      const res = await fetch('/answer?index_id=' + indexId);
-      const data = await res.json();
-      setAnswers(data.answer);
-    };
-    callAnswer();
-  }, []);
+  const { data, isLoading } = useGetAnswersQuery({ index_id: indexId });
 
   return (
     <div>
       <h1>ここは質問回答覧</h1>
       <button>回答</button>
-      <div>
-        <AnswerList answers={answers} />
-      </div>
+      <div>{data && <AnswerList answers={data} />}</div>
+      {isLoading ? 'ロード中...' : null}
     </div>
   );
 };
