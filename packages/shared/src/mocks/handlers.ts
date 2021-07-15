e77cc062edc206e7dbeff9d1f73cc53b8f4d940f
shibayaman <shibayaman7374@gmail.com>
2021-07-15 11:28:24 +0900

    Add user/question-listのモック

--- packages/shared/src/mocks/handlers.ts
+++ packages/shared/src/mocks/handlers.ts
@@ -1,11 +1,16 @@
 import { rest } from 'msw';
 import { editUser, getUser, newUser } from './resolvers/user';
-import { newQuestion, getQuestions } from './resolvers/question';
+import {
+  newQuestion,
+  getQuestions,
+  getUserQuestions,
+} from './resolvers/question';
 import { newAnswer, getAnswer } from './resolvers/answer';
 
 export const handlers = [
   rest.get('/user', getUser),
   rest.get('/question', getQuestions),
+  rest.get('/user/question-list', getUserQuestions),
   rest.get('/answer', getAnswer),
   rest.post('/question', newQuestion),
   rest.post('/answer', newAnswer),
