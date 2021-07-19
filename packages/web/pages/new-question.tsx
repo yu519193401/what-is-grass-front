7513389d90bf10f35811cc2800728c0685e14e59
shibayaman <shibayaman7374@gmail.com>
2021-07-19 09:48:45 +0900

    Refactor 質問投稿画面をrhf + yup化

--- packages/web/pages/new-question.tsx
+++ packages/web/pages/new-question.tsx
@@ -1,5 +1,8 @@
 import Layout from '../components/Layout';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { useAddIndexMutation } from '@what-is-grass/shared';
 import { useRouter } from 'next/router';
 
@@ -14,39 +17,42 @@ const getLanguages = () => [
   },
 ];
 
+type FormValue = {
+  index: string;
+  languageId: number;
+};
+
+const newQuestionFormSchema = yup.object({
+  index: yup.string().required(),
+  languageId: yup.number().required(),
+});
+
 const IndexPage: React.FC = () => {
+  const { register, errors, setValue, handleSubmit } = useForm<FormValue>({
+    resolver: yupResolver(newQuestionFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormValue> = ({ index, languageId }) => {
+    addPost({ language_id: languageId, index });
+  };
+
   const router = useRouter();
   const keyword = router.query.keyword as string;
-  const [index, setIndex] = useState(keyword || '');
-  const [languageId, setLanguageId] = useState<string | null>(null);
   const [addPost, { isLoading }] = useAddIndexMutation();
 
   useEffect(() => {
     if (keyword) {
-      setIndex(keyword);
+      setValue('index', keyword);
     }
   }, [keyword]);
-
-  const handleIndexChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIndex(e.target.value);
-  };
-
-  const handleLanguageIdChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLanguageId(e.target.value);
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    addPost({ language_id: +(languageId || 1), index });
-  };
 
   return (
     <Layout title="New Question">
       <h1>知りたい言葉を質問をしよう</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
           この言葉は何語?:
-          <select value={languageId ?? ''} onChange={handleLanguageIdChange}>
+          <select name="languageId" ref={register}>
             {getLanguages().map((l) => (
               <option key={l.id} value={l.id}>
                 {l.language}
@@ -57,9 +63,11 @@ const IndexPage: React.FC = () => {
         <br />
         <label>
           質問:
-          <input type="text" value={index} onChange={handleIndexChange} />
+          <input type="text" name="index" ref={register} />
           とはどういう意味ですか
         </label>
+        <br />
+        {errors.index?.message}
         <br />
         <input
           type="submit"
